add14761ba2a201863c003edd1c7daa0148cdb00
Camryn Reid <camrr1991@gmail.com>
2019-12-12 11:10:32 -0800

    Renders components without correct styles, begins webpack config

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
-app.use(expres.static('public'));
+app.use(express.static('public'));
 app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, ()=> {
   console.log(`Listening on port ${port}...`);
